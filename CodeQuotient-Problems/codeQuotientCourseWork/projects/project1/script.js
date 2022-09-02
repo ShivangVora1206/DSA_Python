d4d4bdc0a02e09e145672ccc0d162dc305d08b92
ShivangVora1206 <69647458+ShivangVora1206@users.noreply.github.com>
2022-09-02 19:31:40 +0530

    updated from lec on 02/09/2023

--- CodeQuotient-Problems/codeQuotientCourseWork/projects/project1/script.js
+++ CodeQuotient-Problems/codeQuotientCourseWork/projects/project1/script.js
@@ -1,11 +1,13 @@
 //spellcheck
+// original sir's project link https://projects.codequotient.com/project/12-08-2022-3p34g825jjcl6qga83j
 var heading = document.getElementById("heading");
 var elements = document.getElementsByTagName("button");
 var button = document.getElementById("tadaaa");
-var button2 = document.getElementById("abra")
-var button3 = document.getElementById("choo")
-var button4 = document.getElementById("sim")
-
+var button2 = document.getElementById("abra");
+var button3 = document.getElementById("choo");
+var button4 = document.getElementById("sim");
+var div = document.getElementById("parent");
+var lastElement = null;
 console.log(elements);
 console.log(button);
 
@@ -34,3 +36,20 @@ button4.addEventListener("click", function () {
 button4.addEventListener("mouseover", function () {
     heading.innerText = "mouse over button 4";
 })
+
+for (var i=0;i<elements.length;i++){
+    var but = elements[i]
+    but.addEventListener("click", fun)
+}
+
+function fun(event){
+    console.log("target", event.target);
+    var element = document.createElement("button");//redundant
+    var element = document.createElement("h1");
+    element.innerHTML = event.target.innerText;
+    if (lastElement){
+        div.removeChild(lastElement);
+    }
+    div.appendChild(element);
+    lastElement = element;
+}
