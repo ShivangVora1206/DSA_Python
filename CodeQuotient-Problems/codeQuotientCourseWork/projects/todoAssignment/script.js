f6d33e2ac878a9779148505c126b72da1f7aabef
ShivangVora1206 <69647458+ShivangVora1206@users.noreply.github.com>
2022-09-07 23:24:29 +0530

    bug fixeddd

--- CodeQuotient-Problems/codeQuotientCourseWork/projects/todoAssignment/script.js
+++ CodeQuotient-Problems/codeQuotientCourseWork/projects/todoAssignment/script.js
@@ -8,14 +8,13 @@ if(tasks){
 
 taskInput.addEventListener("keypress", function (event) {
     if(event.key == "Enter"){
-        console.log("Enter on input area");
         if (taskInput.value == ""){
             taskInput.classList.add("warning");
             return
         }
 
         taskInput.classList.remove("warning");
-        addTasks(taskInput.value);
+        addTasks(taskInput.value, "button");
         
     }
 })
@@ -23,32 +22,28 @@ taskInput.addEventListener("keypress", function (event) {
 
 function addTasks(taskData, identifier){
     var status = 0;
+    if(typeof taskData === "string"){
+        taskData = [taskData, status];
+    }
     var newRow = document.createElement("tr");
     var newData = document.createElement("td");
     var newData2 = document.createElement("td");
-    // var newData3 = document.createElement("td");
     var taskCompleteButton = document.createElement("button");
     var taskDeleteButton = document.createElement("button");
 
     taskCompleteButton.innerText = "Completed";
     taskDeleteButton.innerText = "Delete";
-    
-
-    if(identifier == undefined){
-        newData.innerText = taskData;
-    }else{
 
         newData.innerText = taskData[0];
-    }
 
     newData.classList.add("p");
 
-    if(taskData[1] == 1){
+    if(taskData[1] === 1){
         newData.style.textDecoration = "line-through";
     }
-    if(identifier == undefined){
-        
-        tasks.push([taskData, status]);
+
+    if(identifier === "button"){
+        tasks.push(taskData);
     }
 
     // newData.style.paddingRight = "100px";
@@ -57,26 +52,23 @@ function addTasks(taskData, identifier){
     taskCompleteButton.addEventListener("click", function () {
 
         newData.style.textDecoration = "line-through";
-        index = tasks.indexOf(taskData);
+        let index = tasks.indexOf(taskData);
         tasks[index][1] = 1;
-        // why is this causing a bug??
         localStorage.setItem("tasks", JSON.stringify(tasks))
         
     })
 
     taskDeleteButton.addEventListener("click", function () {
         taskTable.removeChild(newRow);
-        index = tasks.indexOf(taskData);
+        let index = tasks.indexOf(taskData);
         tasks.splice(index, 1);
         localStorage.setItem("tasks", JSON.stringify(tasks))
     })
 
     newData2.appendChild(taskCompleteButton);
     newData2.appendChild(taskDeleteButton);
-    // newData3.appendChild(taskDeleteButton);
     newRow.appendChild(newData);
     newRow.appendChild(newData2);
-    // newRow.appendChild(newData3);
     taskTable.appendChild(newRow);
     localStorage.setItem("tasks", JSON.stringify(tasks))
     taskInput.value = "";
