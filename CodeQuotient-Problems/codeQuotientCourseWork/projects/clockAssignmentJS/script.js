31007b55ec118e33b7e57144ae5ed262ef163a58
ShivangVora1206 <69647458+ShivangVora1206@users.noreply.github.com>
2022-09-11 15:01:41 +0530

    clock project in js

---
+++ CodeQuotient-Problems/codeQuotientCourseWork/projects/clockAssignmentJS/script.js
@@ -0,0 +1,46 @@
+
+var button = document.querySelector("#button");
+var myInterval = setInterval(secondChanger, 1000);
+
+var hh = 0;
+var mm = 0;
+var ss = 0;
+
+function secondChanger() {
+    var hours = document.querySelector("#hours");
+    var minutes = document.querySelector("#minutes");
+    var seconds = document.querySelector("#seconds");
+    ss++;
+    if(ss < 10){
+        seconds.innerHTML = `0${ss}`;
+    }
+    if(ss >= 10 && ss < 60){
+        seconds.innerHTML = `${ss}`;
+    }
+    if(ss >= 60){
+        ss = 0;
+        mm++;
+        seconds.innerHTML = `0${ss}`;
+        minutes.innerHTML = `0${mm}`;
+    }
+    if(mm < 10){
+        minutes.innerHTML = `0${mm}`;
+    }
+    if(mm >= 10 && mm < 60){
+        minutes.innerHTML = `${mm}`;
+    }
+    if(mm >= 60){
+        mm = 0;
+        hh++;
+        minutes.innerHTML = `0${mm}`;
+        hours.innerHTML = `0${hh}`;
+    }
+    if(hh > 59){
+        clearInterval(myInterval);
+    }
+}
+
+
+button.addEventListener("click", function () {
+    clearInterval(myInterval);
+})
