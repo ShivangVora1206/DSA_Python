6c3f6340d03b645a059bf8bf5166c51909de37f0
ShivangVora1206 <69647458+ShivangVora1206@users.noreply.github.com>
2022-09-14 19:17:52 +0530

    set request header

--- CodeQuotient-Problems/codeQuotientCourseWork/projects/apiCalling1/script.js
+++ CodeQuotient-Problems/codeQuotientCourseWork/projects/apiCalling1/script.js
@@ -1,6 +1,7 @@
 var request = new XMLHttpRequest();
 var url = "https://foodbukka.heokuapp.com/api/v1/restaurant";
 request.open("GET", url);
+request.setRequestHeader("");//set request header
 request.send();
 request.addEventListener("load", function () {
     console.log("data recvd");
