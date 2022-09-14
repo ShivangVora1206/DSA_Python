3f1d4412b0f3b8c616f651f40a8a3bdf839e27a0
ShivangVora1206 <69647458+ShivangVora1206@users.noreply.github.com>
2022-09-14 22:48:20 +0530

    working api calls

--- CodeQuotient-Problems/codeQuotientCourseWork/projects/apiCalling1/script.js
+++ CodeQuotient-Problems/codeQuotientCourseWork/projects/apiCalling1/script.js
@@ -1,10 +1,13 @@
 var request = new XMLHttpRequest();
-var url = "https://foodbukka.heokuapp.com/api/v1/restaurant";
+var url = "https://meowfacts.herokuapp.com/";
 request.open("GET", url);
-request.setRequestHeader("");//set request header
+//request.setRequestHeader("");//set request header
 request.send();
 request.addEventListener("load", function () {
     console.log("data recvd");
     console.log(request.responseText);
+    var data = JSON.parse(request.responseText);
+    console.log(data);
+    
 })
 //lecture project link https://projects.codequotient.com/project/12-08-2022-3p34g825jjcl6qga83j
