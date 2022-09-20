0dc37ac14dd669f4da25140170211101bdcc2592
ShivangVora1206 <69647458+ShivangVora1206@users.noreply.github.com>
2022-09-20 11:30:48 +0530

    functioning js for CQ api calls

--- CodeQuotient-Problems/codeQuotientCourseWork/projects/compileCodeWithApi/script.js
+++ CodeQuotient-Problems/codeQuotientCourseWork/projects/compileCodeWithApi/script.js
@@ -1,14 +1,60 @@
-var request = new XMLHttpRequest();
-var url = "";
-request.open("POST", "https://codequotient.com/api/executeCode");
-var data = {
-    "code":"print(\"hello\")",
-    langId:"0"
-}
-request.setRequestHeader("Content-Type","application/json");
-request.send(JSON.stringify(data));
+var url = "https://codequotient.com/api/";
+var codeText = document.querySelector("#code");
+var compileButton = document.querySelector("#compilebutton");
+var outputArea = document.querySelector("#output");
+var dropdown = document.querySelector("#dropdown");
 
-request.addEventListener("load", function () {
-    console.log("data received");
-    console.log(request.responseText);
+
+compileButton.addEventListener("click", function () {
+    var request = new XMLHttpRequest();
+    var data = {
+        "code": codeText.value,
+        langId: dropdown.value
+    }
+
+    request.open("POST", url + "executeCode");
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(JSON.stringify(data));
+    request.addEventListener("load", function () {
+        // console.log(request.responseText);
+        let response = JSON.parse(request.responseText);
+        if (response.error !== undefined) {
+
+            // console.log("error code is null");
+            outputArea.innerHTML = "";
+
+        }
+        else {
+            // console.log("code not null");
+            var interval = setInterval(function () {
+                var request2 = new XMLHttpRequest();
+
+                request2.open("GET", url + "codeResult/" + response.codeId);
+                request2.send();
+                request2.addEventListener("load", function () {
+
+                    let response2 = JSON.parse(request2.responseText);
+                    let responseData = JSON.parse(response2.data);
+                    if (responseData.status === undefined) {
+                        clearInterval(interval);
+                        // console.log(responseData);
+                        if (responseData.output === "") {
+                            outputArea.innerText = responseData.errors;
+                        } else {
+                            outputArea.innerText = responseData.output;
+                        }
+                    }
+
+                })
+            }, 1000);
+
+
+        }
+    })
+
+
 })
+
+
+//https://codequotient.com/api/codeResult/${codeId}
+
